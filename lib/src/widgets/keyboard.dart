16f815fe6311b7591fd475d8de45cf4e9d93cd93
DarkBones <donkerbc@gmail.com>
2020-12-17 19:54:07 +0100

    Keyboard widget

---
+++ lib/src/widgets/keyboard.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Keyboard extends StatelessWidget {
+  final List<String> rows = ['QWERTYUIOP', 'ASDFGHJKL', 'ZXCVBNM'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: rows.map<Widget>((row) => Text(row)),
+      ),
+    );
+  }
+}
